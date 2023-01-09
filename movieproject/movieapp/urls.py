6c84b4a47d6fee1b5130096a7ffc4f84d087c30e
ruma15 <rumadevi88@gmail.com>
2023-01-09 10:53:38 +0300

    added

---
+++ movieproject/movieapp/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+from django.urls import path
+
+app_name = 'movieapp'
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('movie/<int:movie_id>/', views.detail, name='detail'),
+    path('add/', views.add, name='add'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+]
+
